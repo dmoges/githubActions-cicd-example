dee1e571f400c35c0db0e71be84e5d17457e985d
unknown <dmogesde@gmx.net>
2024-09-04 12:31:25 +0200

    GitActions spring boot code

---
+++ src/main/java/de/moges/cicd/test/ci_cd_test/CiCdTestApplication.java
@@ -0,0 +1,33 @@
+package de.moges.cicd.test.ci_cd_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CiCdTestApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to ci-cd testing using GitActions.";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdTestApplication.class, args);
+	}
+
+}
+
+/*
+ * echo "# githubActions-cicd-example" >> README.md
+ * git init
+ * git add README.md
+ * git commit -m "first commit"
+ * git branch -M main
+ * git remote add origin
+ * https://github.com/dmoges/githubActions-cicd-example.git
+ * git push -u origin main
+ * 
+ */
---
+++ src/test/java/de/moges/cicd/test/ci_cd_test/CiCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.moges.cicd.test.ci_cd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
