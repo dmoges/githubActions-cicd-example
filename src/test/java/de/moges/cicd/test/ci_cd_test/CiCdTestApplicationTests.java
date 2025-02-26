48fd72a5865ca53b097c0ed7166fb3ea4d801103
Dawit Moges <dmogesde@gmx.net>
2025-02-26 12:44:44 +0100

    failing test added

--- src/test/java/de/moges/cicd/test/ci_cd_test/CiCdTestApplicationTests.java
+++ src/test/java/de/moges/cicd/test/ci_cd_test/CiCdTestApplicationTests.java
@@ -1,5 +1,7 @@
 package de.moges.cicd.test.ci_cd_test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,7 @@ class CiCdTestApplicationTests {
 	void contextLoads() {
 	}
 
+	void addFailingtest() {
+		assertEquals(1,2);
+	}
 }
